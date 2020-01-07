2a7a95b061d3b5835dc5e68bbe71ead817a322b8
Dor Itzhaki <dori@wix.com>
2020-01-07 16:52:55 +0200

    update to use mongo version 4.0.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mongo:3.0.14
+FROM mongo:4.0.10
 
 RUN apt-get update && apt-get install -y --no-install-recommends openssl && rm -rf /var/lib/apt/lists/*
 
@@ -6,5 +6,6 @@ VOLUME /data
 EXPOSE 27001 27002 27003
 
 COPY setup.sh .
+RUN chmod +x ./setup.sh
 
 CMD ./setup.sh
--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ if [ ! "$(ls -A /data/db1)" ]; then
     mkdir /data/db2
     mkdir /data/db3
 
-    mongod --smallfiles --dbpath /data/db1 &
+    mongod --dbpath /data/db1 &
     MONGO_PID=$!
 
     waitForMongo 27017
@@ -49,11 +49,11 @@ chmod 600 /var/mongo_keyfile
 
 echo "STARTING CLUSTER"
 
-mongod --port 27003 --smallfiles --dbpath /data/db3 --auth --replSet rs0 --keyFile /var/mongo_keyfile  &
+mongod --port 27003 --dbpath /data/db3 --auth --replSet rs0 --keyFile /var/mongo_keyfile --bind_ip=::,0.0.0.0  &
 DB3_PID=$!
-mongod --port 27002 --smallfiles --dbpath /data/db2 --auth --replSet rs0 --keyFile /var/mongo_keyfile  &
+mongod --port 27002 --dbpath /data/db2 --auth --replSet rs0 --keyFile /var/mongo_keyfile --bind_ip=::,0.0.0.0  &
 DB2_PID=$!
-mongod --port 27001 --smallfiles --dbpath /data/db1 --auth --replSet rs0 --keyFile /var/mongo_keyfile  &
+mongod --port 27001 --dbpath /data/db1 --auth --replSet rs0 --keyFile /var/mongo_keyfile --bind_ip=::,0.0.0.0  &
 DB1_PID=$!
 
 waitForMongo 27001 $USERNAME $PASSWORD
